89cba16bb5faa9e640074d4958fd95b73e7ddaef
George Zhao <qzhao@appnexus.com>
2016-12-08 11:45:39 +0000

    updated makejson to make a dict that has fip -> county,state,zip

--- makejson.py
+++ makejson.py
@@ -6,13 +6,36 @@ import re
 statecodes = json.load(open('state_fips.json'))
 zipmap = {}
 
-for i in range(1,11):
+for i in range(1, 11):
     zfile = open('zipctys/zipcty%d' % i)
-    zfile.readline() # skip first line
+    zfile.readline()  # skip first line
     for l in zfile:
-        m = re.match(r"(?P<zip>.{5}).{18}(?P<state>..)(?P<fips>...)", l)       
+        m = re.match(r"(?P<zip>.{5}).{18}(?P<state>..)(?P<fips>...)(?P<county>.*)$", l)
         if m:
             r = m.groupdict()
-            zipmap[r['zip']] = statecodes[r['state']] + r['fips']
 
-print json.dumps(zipmap)
+            county = r['county'].rstrip("\r").rstrip()
+            state = str(r['state'])
+            statecode = str(statecodes[r['state']])
+            new_zip = str(r['zip'])
+
+            fips = statecode + r['fips']
+
+            if fips in zipmap.keys():
+                tmp_zip = zipmap[fips]['zip']
+                tmp_zip.append(new_zip)
+
+            else:
+                tmp_zip = [new_zip]
+
+            tmp_zip = list(set(tmp_zip))
+            tmp_map = {'state': state,
+                       'county': county,
+                       'zip': tmp_zip}
+
+            zipmap[statecode + r['fips']] = tmp_map
+
+            print json.dumps(tmp_map)
+
+with open('fips2counties_v2.json', 'w') as f:
+    f.write(json.dumps(zipmap))
